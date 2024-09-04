7bb827cb8f89b385523bb8c49dd590e50df51277
Godfrey Onyango <godiodhis268@gmail.com>
2024-09-04 05:51:00 +0300

    last

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import {React,useEffect,useState }from "react"; 
+
+function App(){
+    const[image,setImage]=useState([])
+    const[loading,setLoadng]=useState(true)
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response=>response.json())
+        .then(data=>{
+            setImage(data.message)
+            setLoadng(false)
+        })
+    },[]);
+    if(loading){
+        return<p>Loading...</p>
+    }
+    return(
+        <img src={image} alt='A Random Dog'></img>
+    )
+        
+    
+
+}export default App
